f2694056724519872b547b85f922a3fb774cab7b
Peter Huber <peter@huberistech.com>
2020-12-04 16:59:52 -0500

    Added a texture loader (untested)

--- PCH_MetalPlay/AppController.swift
+++ PCH_MetalPlay/AppController.swift
@@ -14,6 +14,23 @@ class AppController: NSObject {
     @IBOutlet weak var mtkView: MTKView!
     var renderer:Renderer!
     
+    func loadTextureUsingMetalKit(url:URL, device:MTLDevice) -> MTLTexture?
+    {
+        let loader = MTKTextureLoader(device: device)
+        
+        do {
+            
+            let texture = try loader.newTexture(URL: url, options: nil)
+            return texture
+        }
+        catch {
+            
+            let alert = NSAlert(error: error)
+            let _ = alert.runModal()
+            return nil
+        }
+    }
+    
     override  func awakeFromNib() {
         
         guard let defaultDevice = MTLCreateSystemDefaultDevice() else
@@ -24,6 +41,8 @@ class AppController: NSObject {
         
         print("My GPU is: \(defaultDevice)")
         self.mtkView.device = defaultDevice
+        
+        self.mtkView.clearColor = MTLClearColorMake(0, 0.5, 1, 1)
         
         guard let tempRenderer = Renderer(mtkView: self.mtkView) else
         {
